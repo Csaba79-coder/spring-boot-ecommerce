cd517de4d4901f5da58f7c2f24b092dbe0b4a5f4
Csaba79-coder <csabavadasz79@gmail.com>
2023-02-25 21:50:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is the backend part of ecommerce app!
+
+## This is my first full stack project using Angular as a frontend
---
+++ src/main/java/com/csaba79coder/ecommerce/persistence/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.csaba79coder.ecommerce.persistence;
+
+import com.csaba79coder.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
